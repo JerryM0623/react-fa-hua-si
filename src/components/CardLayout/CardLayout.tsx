71dae0fa875926df191d9b8e78946c8a513cfeb6
熊熊熊子路 <jiaweima2000@126.com>
2025-04-04 03:41:38 +0800

    feat: 实现寺物登记、法物登记动态展示

---
+++ src/components/CardLayout/CardLayout.tsx
@@ -0,0 +1,32 @@
+import styles from './CardLayout.module.css'
+import NoData from "../NoData/NoData.tsx";
+import type {ListItem} from "../../types/list.types.ts";
+
+interface ListLayoutProps {
+    list: ListItem[];
+}
+
+function CardLayout({ list }: ListLayoutProps) {
+    return (
+        <>
+            {list.length <= 0 ? (
+                <NoData />
+            ) : (
+                <div className={styles.cardWrapper}>
+                    {
+                        list.map(item => {
+                            return (
+                                <div className={styles.gridItem} key={item.id}>
+                                    <span className={styles.gridItemText}>{item.caption || item.name || item.headline}</span>
+                                    <img className={styles.gridItemIcon} src={item.coveImg || item.headImage || item.newsCoverImg} alt="" />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            )}
+        </>
+    )
+}
+
+export default CardLayout;
--- src/pages/SiWuPage.tsx
+++ src/pages/SiWuPage.tsx
@@ -1,8 +1,49 @@
+import CardLayout from "../components/CardLayout/CardLayout.tsx";
+import {useEffect, useState} from "react";
+
+import type {ListItem} from "../types/list.types.ts";
+
 function SiWuPage() {
+
+    const [list, setList] = useState<ListItem[]>([])
+
+    useEffect(() => {
+        try {
+            const getList = async () => {
+                const apiUrl = "https://fsy.zhifo.net.cn/fosiyun/api/v1/operatingFloor/item/list?tenantId=360&type=1"
+
+                const response = await fetch(apiUrl)
+
+                // 发送请求结束但是请求没有完成
+                if (!response.ok) {
+                    throw new Error(`网络错误！状态码: ${response.status}`);
+                }
+
+                const data = await response.json();
+
+                if (data.status !== 0) {
+                    throw new Error(`获取寺物登记列表数据异常，错误码：${data.status}`);
+                }
+
+                const fetchData = data.data
+
+                if (Array.isArray(fetchData)) {
+                    setList(fetchData)
+                } else {
+                    throw new Error(`获取寺物登记列表数据异常，原因：fetchData 不是数组`);
+                }
+            }
+
+            getList()
+        } catch (e) {
+            console.error("获取寺物登记列表失败，原因：", e)
+        }
+    }, []);
+
     return (
-        <div>
-            <p>这里是寺物的内容。</p>
-        </div>
+        <>
+            <CardLayout list={list} />
+        </>
     );
 }
 
